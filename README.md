eeaafb27723bbd5163453d6688650f2147ae536a
unknown <g_gabrielmerino@yahoo.com.br>
2021-05-03 17:12:48 -0300

    First version

--- README.md
+++ README.md
@@ -1,24 +1,66 @@
-# matadormonstro
+<h1 align="center">Killer of Monsters | VueJS </h1>
+<p align="center">A game made by an RPG fan using the basic concepts of Vue JS. Learning more concepts from Vue Js.</p>
+<p>Project created in the course of Udemy "Curso Vue Js 2 - O guia completo" by <a href="https://github.com/leonardomleitao">Leronardo Leitao Cod3er</a></p>
 
-## Project setup
-```
-npm install
-```
+<h1 align="center">
+    <a href="https://imgbb.com/"><img src="https://i.ibb.co/9VCwfvd/Killer-of-Monster-Logotipo.png" alt="Killer-of-Monster-Logotipo" border="0"></a><br /><a target='_blank' href='https://pt-br.imgbb.com/'></a><br />
+</h1>
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+<h1 align="center">
+    <a href="https://br.vuejs.org/">🔗 VueJs</a>
+</h1>
+<p align="center">A game where you are a killer of monster and must kill all monster with your powers, and special attacks. Use your powers to heal yourself, and finished all monsters.</p>
 
-### Compiles and minifies for production
-```
-npm run build
-```
 
-### Lints and fixes files
-```
-npm run lint
-```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+<p align="center">
+ <a href="#Install">Install instructions</a> • 
+ <a href="#tecnologias">Tecnologias</a> • 
+ <a href="#autor">Autor</a>
+</p>
+
+
+## Install instructions
+
+### Getting Started
+
+#### 1) Clone & Install Dependencies on Linux/MacOS
+
+- 1.1) `git clone `
+- 1.2) `cd killerofmonster` - cd para entrar na pasta criada.
+- 1.3)  Instale Pacotes NPM com `yarn install`
+
+#### 2) Clone & Install Dependencies on Windows
+
+- 2.1) `git clone`
+- 2.2)  Abra o Windows Powershell como Administrador.
+- 2.3) `cd killerofmonster` - cd para entrar na pasta criada.
+- 2.4)  Instale Pacotes com `yarn install` ou `npm install`
+
+#### 3) Start your app
+
+- 3.1) $ npm run serve
+
+## :zap: Tecnologias
+
+<h1 align="center">
+  <img src="https://pnglive.com/wp-content/uploads/2021/01/Vue-JS-Logo-Background-PNG-Image.png" alt="Stack" height="150" width="600">
+  <br>
+</h1>
+
+-   [VueJs](https://github.com/vuejs)
+-   [HTML]
+-   [CSS]
+-   [Javascript]
+
+
+### Autor
+---
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/62441006?v=4" width="100px;" alt=""/>
+ <br />
+ <sub><b>Gabriel Merino</b></sub></a> <a href="https://github.com/Gabri3el/" title="github">🚀</a>
+
+ Feito com maior dedicação por Gabriel Merino!
+
+[![Linkedin Badge](https://img.shields.io/badge/-Gabriel-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/gabrielmerinostos/)](https://www.linkedin.com/in/gabrielmerinostos/)
+[![Gmail Badge](https://img.shields.io/badge/-gabrielmerino.dev@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:gabrielmerino.dev@gmail.com)](mailto:gabrielmerino.dev@gmail.com)
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,283 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="welcome" v-if="!start">
+      <h1>Welcome to Killer of Monsters </h1>
+      <p>Where the strongest survive!!!!</p>
+      <button @click="startGame" class="btn newGame">Iniciar Jogo</button>
+    </div>
+    <div v-if="start" class="panel scores">
+      <div class="score">
+        <h1>Player</h1>
+        <div class="lifebar">
+          <div
+            class="life"
+            :class="{ danger: playerLife <= 20 }"
+            :style="{ width: playerLife + '%' }"
+          ></div>
+          {{ playerLife }}%
+        </div>
+      </div>
+      <div v-if="start" class="score">
+        <h1>Monster</h1>
+        <div class="lifebar">
+          <div
+            class="life"
+            :class="{ danger: monsterLife <= 20 }"
+            :style="{ width: monsterLife + '%' }"
+          ></div>
+        </div>
+        {{ monsterLife }}%
+      </div>
+    </div>
+  <div v-if="start && playerLife == 0 || monsterLife == 0 " class="panel result">
+    <div v-if="monsterLife == 0" class="win">You Win =)</div>
+    <div v-else-if="playerLife == 0" class="lose">You Loseee =(</div>
   </div>
+  <div v-if="start" class="panel buttons">
+    <template  v-if="monsterLife > 0 && playerLife > 0 && start">
+      <button class="btn attack" @click="attack(false)">Attack</button>
+      <button class="btn specialAttack" @click="attack(true)" >Special Attack</button>
+      <button class="btn heal" @click="HealAndHurt">Heal</button>
+    </template>
+    <template v-else-if="monsterLife == 0 || playerLife == 0 && start">
+      <button @click="RestartGame"  class="btn newGame">Play Again</button>
+    </template>
+  </div>
+    
+  <div v-if="start && logs.length" class="panel logs ">
+    <ul>
+      <li v-for="log in logs" v-bind:key="log" :class="log.cls" class="log">
+        {{log.text}}
+      </li>
+    </ul>
+  </div>
+  
+  
+  <div v-if="start" class="start">
+    <button @click="backTitleScreen" class="btn">Voltar</button>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    
+  },
+  data() {
+    return{
+      playerLife: 100,
+      monsterLife: 100,
+      start: false,
+      logs: []  
+    }
+  },
+   methods: {
+        startGame(){
+          this.start = true;
+        },
+        RestartGame(){
+          if(this.playerLife == 0 || this.monsterLife == 0 ){
+          this.playerLife = 100
+          this.monsterLife = 100
+           this.logs = []
+          }
+        },
+        backTitleScreen(){
+          this.start = false;
+        },
+        attack(special){
+          this.hurt('monsterLife',5 ,10 , special, 'Player', 'Monster', 'Player')
+          if(this.monsterLife > 0){
+            this.hurt('playerLife',7 ,12 , false, 'Monster', 'Player','Monster')
+          }
+        },
+        getRandom(min,max){
+          const value = Math.random() * (max - min) + min
+          return Math.round(value)
+        },
+        hurt(prop, min, max, special, source , target, cls){
+          const plus = special ? 5 : 0
+          const hurt = this.getRandom(min + plus, max + plus)
+          this[prop] = Math.max(this[prop] - hurt, 0)
+          this.registerLogs(`${source} atingiu ${target} com ${hurt}.`,cls)
+        },
+        HealAndHurt(){
+          this.heal(10,15)
+          this.hurt('playerLife',7 ,12 , false , 'Monster', 'Player','Monster')
+        },
+        heal(max, min){
+          const heal = this.getRandom(min,max)
+          this.playerLife = Math.min(this.playerLife + heal, 100)
+          this.registerLogs(`player receives ${heal} of Heal`,'Player')
+        },
+        registerLogs(text , cls){
+          this.logs.unshift({text,cls})
+          console.log(this.logs)
+        }
+  }, 
+};
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+html {
+  font-family: 'Press Start 2P', cursive;
+  background-image: url('./assets/background.jpg');
+  color: #fff;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    display: flex;
+    flex-direction: column;
+}
+.welcome{
+  margin: 100px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.start{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.start button{
+  border: 2px solid green;
+  border-radius: 10px;
+  background-color: green;
+  color: aliceblue;
+  width: 200px;
+  height: 50px;
+  cursor: pointer;
+  font-family: 'Press Start 2P', cursive;
+}
+
+.panel {
+    margin: 10px;
+    padding: 20px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+    border: 2px solid white;
+}
+
+
+/* Scores */
+
+.scores {
+    display: flex;
+}
+
+.score {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.score h1 {
+ font-size: 20px;
+}
+
+.lifebar {
+    width: 70%;
+    height: 30px;
+    border: 2px solid #AAA;
+    margin: 0;
+}
+
+.lifebar .life {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    background-color: green;
+}
+
+.lifebar .life.danger {
+    background-color: red;
+}
+/*Result */
+
+
+.result{
+  display: flex;
+  justify-content: center;
+  font-size: 1.3rem;
+  font-weight: 600;
+}
+
+.result .win {
+  color: green;
+}
+
+.result .lose{
+  color: red;
+}
+
+
+/*Game Button*/
+.btn{
+  padding: 10px 50px;
+  margin: 0 65px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  font-size: 0.8rem;
+  cursor: pointer;
+  font-family: 'Press Start 2P', cursive;
+}
+.btn button{
+  display: flex;
+  justify-content: center;
+}
+
+.newGame{
+  background-color: #4253af;
+  color:aliceblue;
+  margin: 0
+}
+
+.attack{
+  background-color: red;
+  color: aliceblue;
+}
+.specialAttack{
+  background-color: rgb(27, 21, 12);
+  color: aliceblue;
+}
+.heal{
+  background-color: green;
+  color: aliceblue;
+}
+
+
+/* LOGS*/
+
+.logs ul {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  padding: 0;
+  margin: 0 
+}
+
+
+.logs ul li{
+  display: flex;
+  justify-content: center;
+  margin: 4px 0px;
+  padding: 3px 0px;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+  border-radius: 3px;
+}
+
+.Player{
+  background-color: #4253afaa;
+  color: #fff;
+}
+
+.Monster{
+  background-color: #e51c23aa;
+  color: #fff
 }
 </style>
